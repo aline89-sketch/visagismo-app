66eba180583da80fab1ecf695cf35e997cffec87
aline89-sketch <aline89@gmail.com>
2026-06-06 16:48:58 -0300

    Delete api/analyze.js

--- api/analyze.js
+++
@@ -1,3 +0,0 @@
-const prompt = `Retorne exatamente este JSON preenchido com números inteiros:
-
-{"s":{"ov":50,"re":50,"qu":50,"ob":50,"co":50,"ti":50,"di":50,"tr":50},"p":{"al":50,"an":50,"ps":50,"pi":50,"dm":50,"aq":50}}`;
