44ac52e818dc45f15d73a76bd192f75b704fd598
aline89-sketch <aline89@gmail.com>
2026-06-06 12:33:42 -0300

    Update analyze.js

--- api/analyze.js
+++ api/analyze.js
@@ -43,7 +43,7 @@ Legenda perfil: al=alongamento an=angularidade ps=predominancia_superior pi=pred
       }],
      generationConfig: {
   temperature: 0,
-  maxOutputTokens: 256,
+  maxOutputTokens: 1024,
   responseMimeType: "application/json"
 }
     };
